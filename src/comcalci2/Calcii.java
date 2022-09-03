af1d741de0e2c1aea9e8d779c864b637c97a6860
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-03 11:02:22 +0530

    Added:Complete project

---
+++ src/comcalci2/Calcii.java
@@ -0,0 +1,27 @@
+package comcalci2;
+
+public class Calcii {
+
+	public static void name(String args []) {
+		
+		System.out.println("We are doing calcultor program");
+		  
+		   int num1=20;
+		   int num2=30;
+		   
+		   
+		   int sum=num1+num2;
+		   int sub=num1-num2;
+		   int mul=num1*num2;
+		   int div=num1/num2;
+		   System.out.println("sum is: "+sum);
+		   System.out.println("sub is: "+sub);
+		   System.out.println("mul is: "+mul);
+		   System.out.println("div is: "+div);
+		    
+	}
+
+
+
+	}
+
